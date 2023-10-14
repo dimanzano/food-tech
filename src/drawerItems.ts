ef8ea8c14d31f3e0e8937fb24c99a74886f07f68
Diana Manzano <dimanzanoo@gmail.com>
2023-10-14 09:10:09 +0200

    change icons + added supermarkets

--- src/drawerItems.ts
+++ src/drawerItems.ts
@@ -2,13 +2,15 @@ import {
     Speed,
     SignalCellularAlt,
     RestaurantMenu,
-    Badge
+    Badge,
+    TakeoutDining
   } from "@mui/icons-material";
   
   export const drawerItems = [
     { label: "Dashboard", icon: Speed, href: "/" },
     { label: "Data Collection", icon: SignalCellularAlt, href: "/data" },
-    { label: "Restaurants", icon: RestaurantMenu, href: "/restaurants" },
+    { label: "Restaurants", icon: TakeoutDining, href: "/restaurants" },
+    { label: "Supermarkets", icon: RestaurantMenu, href: "/supermarkets" },
     { label: "Memberships", icon: Badge, href: "/memberships" },
   ];
  
