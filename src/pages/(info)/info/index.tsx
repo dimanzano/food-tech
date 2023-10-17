855a1126f5a72cd4592685d4f961171c8e7b6404
Diana Manzano <dimanzanoo@gmail.com>
2023-10-17 22:14:25 +0200

    about section

--- src/pages/(info)/info/index.tsx
+++ src/pages/(info)/info/index.tsx
@@ -1,5 +1,8 @@
 import { Box, Button, Typography } from "@mui/material";
 import UpperBarSection from "../../../components/info/UpperBarSection";
+import TuneIcon from "@mui/icons-material/Tune";
+import FastfoodIcon from "@mui/icons-material/Fastfood";
+import FavoriteIcon from "@mui/icons-material/Favorite";
 
 export default function InfoPage() {
   return (
@@ -16,95 +19,59 @@ export default function InfoPage() {
           <img src="/src/assets/images/beef.png" alt="beef" width="100%" />
         </Box>
 
-        {/* Text */}
         <Box sx={{ width: "50%", padding: "40px" }}>
           <Typography variant="h4">Lorem ipsum dolor</Typography>
           <Typography marginBottom={5}>Lorem ipsum</Typography>
 
           <Box flexDirection={"row"} gap={1.5} display={"flex"}>
-            <Button variant="outlined">Login</Button>
-            <Button variant="contained">Sign Up</Button>
+            <Button variant="contained" color="success">
+              Get Started
+            </Button>
+            <Button variant="outlined" color="success">
+              Sign Up
+            </Button>
           </Box>
         </Box>
       </Box>
 
       {/* Landscape Page Sections */}
-      <Box id="about-us" sx={{ p: 4, backgroundColor: "grey.300" }}>
+      <Box
+        id="about-us"
+        sx={{ p: 4, backgroundColor: "grey.300", textAlign: "center" }}
+      >
         <Typography variant="h4">About Us</Typography>
-        <Typography>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, ipsam?
-          m dolor sit amet consectetur adipisicing elit. Quos, ipsam? m dolor
-          sit amet consectetur adipisicing elit. Quos, ipsam? m dolor sit amet
-          consectetur adipisicing elit. Quos, ipsam? m dolor sit amet
-          consectetur adipisicing elit. Quos, ipsam? m dolor sit amet
-          consectetur adipisicing elit. Quos, ipsam? m dolor sit amet
-          consectetur adipisicing elit. Quos, ipsam? m dolor sit amet
-          consectetur adipisicing elit. Quos, ipsam? m dolor sit amet
-          consectetur adipisicing elit. Quos, ipsam? m dolor sit amet
-          consectetur adipisicing elit. Quos, ipsam? m dolor sit amet
-          consectetur adipisicing elit. Quos, ipsam? m dolor sit amet
-          consectetur adipisicing elit. Quos, ipsam? m dolor sit amet
-          consectetur adipisicing elit. Quos, ipsam? m dolor sit amet m dolor
-          sit amet consectetur adipisicing elit. Quos, ipsam? m dolor sit amet
-          consectetur adipisicing elit. Quos, ipsam? m dolor sit amet
-          consectetur adipisicing elit. Quos, ipsam? m dolor sit amet
-          consectetur adipisicing elit. Quos, ipsam? m dolor sit amet
-          consectetur adipisicing elit. Quos, ipsam? m dolor sit amet
-          consectetur adipisicing elit. Quos, ipsam? m dolor sit amet
-          consectetur adipisicing elit. Quos, ipsam? m dolor sit amet
-          consectetur adipisicing elit. Quos, ipsam? m dolor sit amet
-          consectetur adipisicing elit. Quos, ipsam? m dolor sit amet
-          consectetur adipisicing elit. Quos, ipsam? m dolor sit amet
-          consectetur adipisicing elit. Quos, ipsam? m dolor sit amet
-          consectetur adipisicing elit. Quos, ipsam? m dolor sit amet
-          consectetur adipisicing elit. Quos, ipsam? m dolor sit amet
-          consectetur adipisicing elit. Quos, ipsam? m dolor sit amet
-          consectetur adipisicing elit. Quos, ipsam? m dolor sit amet
-          consectetur adipisicing elit. Quos, ipsam? m dolor sit amet
-          consectetur adipisicing elit. Quos, ipsam? consectetur adipisicing
-          ipsam? Quos, ipsam? it amet consectetur adipisicing elit. Quos,
-          ipsam?Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos,
-          ipsam? it amet consectetur adipisicing elit. Quos, ipsam? Quos, ipsam?
-          it amet consectetur adipisicing elit. Quos, ipsam? Quos, ipsam? it
-          amet consectetur adipisicing elit. Quos, ipsam?Lorem ipsum dolor sit
-          amet consectetur adipisicing elit. Quos, ipsam? it amet consectetur
-          adipisicing elit. Quos, ipsam? Quos, ipsam? it amet consectetur
-          adipisicing elit. Quos, ipsam? Quos, ipsam? it amet consectetur
-          adipisicing elit. Quos, ipsam?Lorem ipsum dolor sit amet consectetur
-          adipisicing elit. Quos, ipsam? it amet consectetur adipisicing elit.
-          Quos, ipsam? Quos, ipsam? it amet consectetur adipisicing elit. Quos,
-          ipsam? Quos, ipsam? it amet consectetur adipisicing elit. Quos,
-          ipsam?Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos,
-          ipsam? it amet consectetur adipisicing elit. Quos, ipsam? Quos, ipsam?
-          it amet consectetur adipisicing elit. Quos, ipsam? Quos, ipsam? it
-          amet consectetur adipisicing elit. Quos, ipsam?Lorem ipsum dolor sit
-          amet consectetur adipisicing elit. Quos, ipsam? it amet consectetur
-          adipisicing elit. Quos, ipsam? Quos, ipsam? it amet consectetur
-          adipisicing elit. Quos, ipsam? Quos, ipsam? it amet consectetur
-          adipisicing elit. Quos, ipsam?Lorem ipsum dolor sit amet consectetur
-          adipisicing elit. Quos, ipsam? it amet consectetur adipisicing elit.
-          Quos, ipsam? Quos, ipsam? it amet consectetur adipisicing elit. Quos,
-          ipsam? Quos, ipsam? it amet consectetur adipisicing elit. Quos,
-          ipsam?Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos,
-          ipsam? it amet consectetur adipisicing elit. Quos, ipsam? Quos, ipsam?
-          it amet consectetur adipisicing elit. Quos, ipsam? Quos, ipsam? it
-          amet consectetur adipisicing elit. Quos, ipsam?Lorem ipsum dolor sit
-          amet consectetur adipisicing elit. Quos, ipsam? it amet consectetur
-          adipisicing elit. Quos, ipsam? Quos, ipsam? it amet consectetur
-          adipisicing elit. Quos, ipsam? Quos, ipsam? it amet consectetur
-          adipisicing elit. Quos, ipsam?Lorem ipsum dolor sit amet consectetur
-          adipisicing elit. Quos, ipsam? it amet consectetur adipisicing elit.
-          Quos, ipsam? Quos, ipsam? it amet consectetur adipisicing elit. Quos,
-          ipsam? Quos, ipsam? it amet consectetur adipisicing elit. Quos,
-          ipsam?Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos,
-          ipsam? it amet consectetur adipisicing elit. Quos, ipsam? Quos, ipsam?
-          it amet consectetur adipisicing elit. Quos, ipsam? Quos, ipsam? it
-          amet consectetur adipisicing elit. Quos, ipsam?Lorem ipsum dolor sit
-          amet consectetur adipisicing elit. Quos, ipsam? it amet consectetur
-          adipisicing elit. Quos, ipsam? Quos, ipsam? it amet consectetur
-          adipisicing elit. Quos, ipsam? Quos, ipsam? it amet consectetur
-          adipisicing elit. Quos, ipsam?
-        </Typography>
+
+        <Box sx={{ display: "flex", justifyContent: "center" }}>
+          {/* Column 1 */}
+          <Box sx={{ textAlign: "center", padding: "20px" }}>
+            <TuneIcon sx={{ fontSize: 80 }} color="success" />
+            <Typography variant="h6">Title 1</Typography>
+            <Typography variant="body1">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos,
+              ipsam? it amet consectetur adipisicing elit.{" "}
+            </Typography>
+          </Box>
+
+          {/* Column 2 */}
+          <Box sx={{ textAlign: "center", padding: "20px" }}>
+            <FastfoodIcon sx={{ fontSize: 80 }} />
+            <Typography variant="h6">Title 2</Typography>
+            <Typography variant="body1">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos,
+              ipsam? it amet consectetur adipisicing elit.{" "}
+            </Typography>
+          </Box>
+
+          {/* Column 3 */}
+          <Box sx={{ textAlign: "center", padding: "20px" }}>
+            <FavoriteIcon sx={{ fontSize: 80 }} color="error" />
+            <Typography variant="h6">Title 3</Typography>
+            <Typography variant="body1">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos,
+              ipsam? it amet consectetur adipisicing elit.{" "}
+            </Typography>
+          </Box>
+        </Box>
       </Box>
 
       <Box id="how-it-works" sx={{ p: 4, backgroundColor: "grey.200" }}>
