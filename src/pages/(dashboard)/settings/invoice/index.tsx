f3c85d528e6d65f37136606d92aa3ebf90537870
Diana Manzano <dimanzanoo@gmail.com>
2023-10-24 16:36:30 +0200

    add body to invoice

--- src/pages/(dashboard)/settings/invoice/index.tsx
+++ src/pages/(dashboard)/settings/invoice/index.tsx
@@ -1,11 +1,38 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Divider, Typography } from "@mui/material";
 
 export default function SettingsInvoicePage() {
   return (
-    <>
-      <Box>
-        <Typography> invoice?</Typography>
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        padding: 4,
+        maxWidth: 600,
+        margin: "auto",
+        backgroundColor: "grey.100",
+        gap: 2,
+      }}
+    >
+      <Typography variant="h5" textAlign="center" fontWeight={"bold"}>
+        Invoice
+      </Typography>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          gap: 7,
+        }}
+      >
+        {/* Current Plan Section */}
+        <Box sx={{ display: "flex", flexDirection: "column", gap: 1 }}>
+          <Typography variant="overline">Current Plan</Typography>
+          <Divider />
+          <Typography variant="h6" fontWeight={"bold"}>
+            Basic Plan
+          </Typography>
+          <Typography variant="body1"> $9.99/month</Typography>
+        </Box>
       </Box>
-    </>
+    </Box>
   );
 }
