fb0185b3be02eafc332a4d6e24f9d8f2cf40162a
Diana Manzano <dimanzanoo@gmail.com>
2023-10-25 17:17:00 +0200

    invoice section done

--- src/pages/(dashboard)/settings/invoice/index.tsx
+++ src/pages/(dashboard)/settings/invoice/index.tsx
@@ -1,4 +1,20 @@
-import { Box, Divider, Typography } from "@mui/material";
+import {
+  Box,
+  Typography,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from "@mui/material";
+
+// Mock Data
+const invoiceData = [
+  { id: 1, planName: "Basic Plan", price: "$9.99", datePayed: "01-10-2023" },
+  { id: 2, planName: "Pro Plan", price: "$19.99", datePayed: "01-09-2023" },
+  { id: 3, planName: "Premium Plan", price: "$29.99", datePayed: "01-08-2023" },
+];
 
 export default function SettingsInvoicePage() {
   return (
@@ -23,15 +39,29 @@ export default function SettingsInvoicePage() {
           gap: 7,
         }}
       >
-        {/* Current Plan Section */}
-        <Box sx={{ display: "flex", flexDirection: "column", gap: 1 }}>
-          <Typography variant="overline">Current Plan</Typography>
-          <Divider />
-          <Typography variant="h6" fontWeight={"bold"}>
-            Basic Plan
-          </Typography>
-          <Typography variant="body1"> $9.99/month</Typography>
-        </Box>
+        {/* Invoice Table */}
+        <TableContainer>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>Id</TableCell>
+                <TableCell>Plan Name</TableCell>
+                <TableCell>Price</TableCell>
+                <TableCell>Date Paid</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {invoiceData.map((invoice) => (
+                <TableRow key={invoice.id}>
+                  <TableCell>{invoice.id}</TableCell>
+                  <TableCell>{invoice.planName}</TableCell>
+                  <TableCell>{invoice.price}</TableCell>
+                  <TableCell>{invoice.datePayed}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
       </Box>
     </Box>
   );
