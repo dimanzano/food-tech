cd1d5dd6756a8f3291d82b5ab8d0624afe7dc7a0
Diana Manzano <dimanzanoo@gmail.com>
2023-10-26 11:50:28 +0200

    added details to restaurant page

--- src/mockData/restaurantData.ts
+++ src/mockData/restaurantData.ts
@@ -11,15 +11,15 @@ export const restaurantData: Restaurant[] = [
   {
     id: 1,
     name: "Restaurant 1",
-    image: "https://via.placeholder.com/150", // replace with actual image url
+    image: "/src/assets/images/restaurants/breakfast-rest.png",
     description: "This is a description for Restaurant 1",
-    specialties: ["Specialty 1", "Specialty 2"],
+    specialties: ["Specialty 1"],
     address: "123 Main St, City, State, 12345",
   },
   {
     id: 2,
     name: "Restaurant 2",
-    image: "https://via.placeholder.com/150", // replace with actual image url
+    image: "/src/assets/images/restaurants/sushi-rest.jpg",
     description: "This is a description for Restaurant 2",
     specialties: ["Specialty 1", "Specialty 2", "Specialty 3"],
     address: "123 Main St, City, State, 12345",
@@ -27,7 +27,7 @@ export const restaurantData: Restaurant[] = [
   {
     id: 3,
     name: "Restaurant 3",
-    image: "https://via.placeholder.com/150", // replace with actual image url
+    image: "/src/assets/images/restaurants/burger-rest.jpg",
     description: "This is a description for Restaurant 2",
     specialties: ["Specialty 1", "Specialty 2", "Specialty 3"],
     address: "123 Main St, City, State, 12345",
@@ -35,7 +35,7 @@ export const restaurantData: Restaurant[] = [
   {
     id: 4,
     name: "Restaurant 4",
-    image: "https://via.placeholder.com/150", // replace with actual image url
+    image: "/src/assets/images/restaurants/toast-rest.jpg",
     description: "This is a description for Restaurant 4",
     specialties: ["Specialty 1", "Specialty 2"],
     address: "123 Main St, City, State, 12345",
@@ -43,7 +43,7 @@ export const restaurantData: Restaurant[] = [
   {
     id: 5,
     name: "Restaurant 5",
-    image: "https://via.placeholder.com/150", // replace with actual image url
+    image:  "/src/assets/images/restaurants/pasta-rest.jpg",
     description: "This is a description for Restaurant 5",
     specialties: ["Specialty 1", "Specialty 2", "Specialty 3"],
     address: "123 Main St, City, State, 12345",
@@ -51,7 +51,7 @@ export const restaurantData: Restaurant[] = [
   {
     id: 6,
     name: "Restaurant 6",
-    image: "https://via.placeholder.com/150", // replace with actual image url
+    image: "/src/assets/images/restaurants/poke-rest.jpg",
     description: "This is a description for Restaurant 6",
     specialties: ["Specialty 1", "Specialty 2", "Specialty 3"],
     address: "123 Main St, City, State, 12345",
