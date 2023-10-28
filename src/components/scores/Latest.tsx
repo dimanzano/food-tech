95cd6b14bf61ad6b9bb260fc4bad12abeca0584a
Diana Manzano <dimanzanoo@gmail.com>
2023-10-28 13:39:40 +0200

    added graphs

---
+++ src/components/scores/Latest.tsx
@@ -0,0 +1,82 @@
+import { Box, Typography } from "@mui/material";
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
+
+const foodData = [
+  { name: "Spicy", uv: 400, amt: 2400 },
+  { name: "Sweet", uv: 350, amt: 2210 },
+  { name: "Salty", uv: 200, amt: 2290 },
+  { name: "Vegetables", uv: 200, amt: 2000 },
+  { name: "Vegetables", uv: 300, amt: 2000 },
+  { name: "Vegetables", uv: 270, amt: 2000 },
+  { name: "Vegetables", uv: 200, amt: 2000 },
+  { name: "Vegetables", uv: 300, amt: 2000 },
+  // add more categories...
+];
+
+const qualityData = [
+  { name: "Nutritional Labeling", uv: 400, amt: 2400 },
+  { name: "Hygiene and Safety", uv: 350, amt: 2210 },
+  { name: "Organic", uv: 200, amt: 2290 },
+  { name: "Sustainability", uv: 200, amt: 2000 },
+  { name: "Preference Testing", uv: 300, amt: 2000 },
+  // add more categories...
+];
+
+export default function LatestSection() {
+  return (
+    <Box marginTop={2} sx={{ height: "50vh", width: "90%" }}>
+      <Typography variant="h6" fontWeight={"bold"}>
+        Food Types
+      </Typography>
+      <ResponsiveContainer width="100%" height="100%">
+        <BarChart
+          data={foodData}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Bar dataKey="uv" fill="#438C2D" />
+        </BarChart>
+      </ResponsiveContainer>
+
+      <Typography variant="h6" fontWeight={"bold"}>
+        Food Quality
+      </Typography>
+      <ResponsiveContainer width="100%" height="100%">
+        <BarChart
+          data={qualityData}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Bar dataKey="uv" fill="#101051" />
+        </BarChart>
+      </ResponsiveContainer>
+    </Box>
+  );
+}
--- src/pages/(dashboard)/index.tsx
+++ src/pages/(dashboard)/index.tsx
@@ -1,5 +1,32 @@
-import { Typography } from "@mui/material";
+import React, { useState } from "react";
+import { Typography, Tabs, Tab, Box } from "@mui/material";
+import LatestSection from "../../components/scores/Latest";
+import PastSection from "../../components/scores/Past";
 
 export default function DashboardPage() {
-  return <Typography variant="h4">Dashboard</Typography>;
+  const [value, setValue] = useState(0);
+
+  const handleChange = (_event: React.ChangeEvent<{}>, newValue: number) => {
+    setValue(newValue);
+  };
+
+  return (
+    <Box>
+      <Typography variant="h4">Personalized Food Scores</Typography>
+      <Tabs value={value} onChange={handleChange}>
+        <Tab label="Latest Scores" />
+        <Tab label="Past Scores" />
+      </Tabs>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          gap: 2,
+        }}
+      >
+        {value === 0 && <LatestSection />}
+        {value === 1 && <PastSection />}
+      </Box>
+    </Box>
+  );
 }
